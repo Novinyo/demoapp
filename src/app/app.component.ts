f37990b923ab228bc5c1332fd2995eeceadb2085
Kodjo Novinyo AMEGADJE <novinyolearn@gmail.com>
2018-11-15 10:59:31 +0000

    Initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'demoapp';
+  FirstNum = 0;
+  SecondNum = 0;
+
+  Result: Number = 0;
+  constructor() {
+
+  }
+
+  Calculate (val1, val2) {
+    this.Result = this.FirstNum + this.SecondNum;
+  }
 }
